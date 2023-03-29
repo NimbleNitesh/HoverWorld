f7ed087b3fc48cc9e76c72f4e82fe4479eb55210
AnushDubey <anush20.12.20@gmail.com>
2023-03-29 13:33:30 +0530

    dashboard initial edit

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {useList} from '@pankod/refine-core'
+import { Typography,Box, Stack } from '@pankod/refine-mui'
+
+import{
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from'components'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+         title="Hotels Available" 
+         value={54}
+         series={[54,46]}
+         color={['#475be8','#e4e8ef']}
+         />
+
+      </Box>
+    </Box>
   )
 }
 
