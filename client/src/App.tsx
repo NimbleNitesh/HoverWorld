4fd0f470c28dfeaf5ebca85eba3929f0d4464db4
NimbleNitesh <nitesh620137@gmail.com>
2023-04-02 00:20:29 +0530

    corrected localhost url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 
       // Save User to MongoDB ....
       if(profileObj) {
-        const response= await fetch('http://localhost/8080/api/v1/users', {
+        const response= await fetch('http://localhost:8080/api/v1/users', {
           method:'POST',
           headers: {'Content-Type': 'application/json'},
           body:JSON.stringify({
