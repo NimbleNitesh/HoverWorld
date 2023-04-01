fcbd8e7796ac46e65871e05a60fdc9d3646231ab
AnushDubey <anush20.12.20@gmail.com>
2023-04-01 22:38:57 +0530

    Create items Form

--- client/src/components/common/Form.tsx
+++ client/src/components/common/Form.tsx
@@ -1,8 +1,44 @@
 import React from 'react'
-
-const Form = () => {
+import {
+    Box,
+    Typography,
+    FormControl,
+    FormHelperText,
+    TextField,
+    TextareaAutosize,
+    Stack,
+    Select,
+    MenuItem,
+    Button,
+} from "@mui/material";
+import { FormProps } from "interfaces/common";
+import CustomButton from "./CustomButton";
+const Form = ({type,
+    register,
+    handleSubmit,
+    handleImageChange,
+    formLoading,
+    onFinishHandler,
+    propertyImage,
+  }:FormProps) => {
   return (
-    <div>Form</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        {type} a Property
+      </Typography>
+       <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+                <form
+                    style={{
+                        marginTop: "20px",
+                        width: "100%",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "20px",
+                    }}
+                    onSubmit={handleSubmit(onFinishHandler)}
+                    >
+                </form>
+    </Box>
   )
 }
 
